4e603656f1381e4fab1e43468bfaaa7cb71170d5
Andrew Fore <andrew.fore@newfold.com>
2025-06-13 14:14:53 -0400

    Updated for paths

--- disko-flake/flake.nix
+++ disko-flake/flake.nix
@@ -6,10 +6,9 @@
     nixosConfigurations.asus-amd-laptop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./hosts/asus-amd-laptop/disko.nix
+        ../hosts/asus-amd-laptop/disko.nix
         disko.nixosModules.disko
       ];
     };
   };
 }
-
