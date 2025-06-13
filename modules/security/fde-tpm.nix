d162c8b7f0ed53ac7a10336f042dcb616be16bf2
Andrew Fore <andrew.fore@newfold.com>
2025-06-13 14:28:22 -0400

    Delaying tpm2

--- modules/security/fde-tpm.nix
+++ modules/security/fde-tpm.nix
@@ -3,7 +3,7 @@
   boot.initrd.systemd.enable = true;
   boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-partlabel/luks";
   boot.initrd.luks.devices."cryptroot".tpm2 = true;
-  security.tpm2.enable = true;
-  security.tpm2.tss2.enable = true;
+  # security.tpm2.enable = true;
+  # security.tpm2.tss2.enable = true;
   environment.systemPackages = with pkgs; [ tpm2-tools ];
 }
