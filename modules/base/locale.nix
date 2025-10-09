eecad41b26ce7ab9f4beb0f2a5e4a3e485c6f8a7
Andrew Fore <andrew.fore@newfold.com>
2025-10-09 14:06:54 -0400

    Starting over

--- modules/base/locale.nix
+++
@@ -1,6 +0,0 @@
-{ config, ... }:
-{
-  i18n.defaultLocale = "en_US.UTF-8";
-  console.keyMap = "us";
-  time.timeZone = "America/New_York";
-}
--- modules/hardware/pc-laptop.nix
+++
@@ -1,6 +0,0 @@
-{ config, pkgs, ... }:
-{
-  boot.initrd.kernelModules = [ "kvm-amd" "amdgpu" ];
-  services.fwupd.enable = true;
-  services.tlp.enable = true;
-}
