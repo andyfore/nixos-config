540457b494d79d2f7fc03b25c94748dbcf63b7c3
Andrew Fore <andrewrfore@gmail.com>
2025-10-10 01:21:58 +0000

    Updated content

--- home/andyfore.nix
+++ home/andyfore.nix
@@ -14,10 +14,45 @@
         alacritty git curl unzip kitty brave
         # Audio/display tools
         pavucontrol brightnessctl
+        neovim zsh
     ];
+
+    # Zsh (example program module)
+    programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        autosuggestions.enable = true;
+        syntaxHighlighting.enable = true;
+        # If you keep your .zshrc in chezmoi, don’t set interactiveShellInit here.
+    };
+  
+    programs.git = {
+        enable = true;
+        userName  = "Andrew Fore";
+        userEmail = "andrewrfore@gmail.com";
+        extraConfig = {
+            init.defaultBranch = "main";
+            pull.rebase = true;
+        };
+    };
 
     # (Optional quality-of-life)
     programs.git.enable = true;
     programs.alacritty.enable = true;
     programs.waybar.enable = false; # leave false if you use your own config file
+
+  # Fonts & theming are fine to manage here if you want
+  fonts.fontconfig.enable = true;
+
+  # XDG defaults: lets you reference dotfiles cleanly
+  xdg.enable = true;
 }
+
+
+  # User services example (disabled by default)
+  # systemd.user.services."example-timer" = {
+  #   Unit.Description = "Example user service";
+  #   Service = { ExecStart = "${pkgs.coreutils}/bin/true"; };
+  #   Install.WantedBy = [ "default.target" ];
+  # };
+
