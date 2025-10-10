350f1804e5acabb2a04c2c3fc324854e5f3b1002
Andrew Fore <andrewrfore@gmail.com>
2025-10-10 01:26:05 +0000

    Removed home-manager integration

--- flake.nix
+++ flake.nix
@@ -4,11 +4,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+#    home-manager.url = "github:nix-community/home-manager";
+#    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, disko, home-manager, ... }: {
+  outputs = { self, nixpkgs, ... }: {
     nixosConfigurations = {
       asus-amd-laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -16,20 +16,12 @@
           ./hosts/asus-amd-laptop/default.nix
           ./hosts/asus-amd-laptop/hardware-configuration.nix
   
-  #        ./modules/base/locale.nix
-  #        ./modules/base/networking.nix
-  #        ./modules/base/nix.nix
-  #        ./modules/base/users.nix
-  #        ./modules/desktop/hyprland.nix
-  #        ./modules/desktop/sddm.nix
-  #        ./modules/hardware/amd.nix
-  
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.andyfore = import ./home/andyfore.nix;
-          }
+#          home-manager.nixosModules.home-manager
+#          {
+#            home-manager.useGlobalPkgs = true;
+#            home-manager.useUserPackages = true;
+#            home-manager.users.andyfore = import ./home/andyfore.nix;
+#          }
         ];
       };
     };
