66c10b3320a08a83262301816f8ad891228c8556
Andrew Fore <andrewrfore@gmail.com>
2025-11-03 00:59:41 +0000

    Possible syntax fix

--- hosts/asus-amd-laptop/default.nix
+++ hosts/asus-amd-laptop/default.nix
@@ -7,7 +7,7 @@
     ../../modules/base/nix.nix
     ../../modules/base/packages.nix
     ../../modules/base/users.nix
-    ../../modules/desktop/hyprland/
+    ../../modules/desktop/hyprland
     ../../modules/desktop/sddm.nix
     ../../modules/hardware/amd-laptop.nix
   ];
