96fd32256de919a9c2b2240e56384a8476e3204a
Andrew Fore <andrewrfore@gmail.com>
2025-11-04 21:58:55 +0000

    Updated file path

--- hosts/asus-amd-laptop/default.nix
+++ hosts/asus-amd-laptop/default.nix
@@ -7,7 +7,7 @@
     ../../modules/base/nix.nix
     ../../modules/base/packages.nix
     ../../modules/base/users.nix
-    ../../modules/desktop/hyprland
+    ../../modules/home/hyprland
     ../../modules/desktop/sddm.nix
     ../../modules/hardware/amd-laptop.nix
   ];
