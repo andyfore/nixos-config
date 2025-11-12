9672288f34d4a849f43a9d1d5003b3340833b7a9
Andrew Fore <andrew.fore@newfold.com>
2025-11-12 13:26:32 -0500

    Updated pkgs and vars

--- modules/home/andyfore.nix
+++ modules/home/andyfore.nix
@@ -14,6 +14,7 @@
         # Audio/display tools
         pavucontrol brightnessctl
         neovim zsh
+        waypaper blueman kdeconnect swayosd
     ];
 
     imports = [
--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -1,6 +1,16 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   wayland.windowManager.hyprland = {
     enable = true;
+    settings = {
+      "exec-once" = [
+        "uwsm app -- nm-applet"
+        "waypaper --restore"
+        "uwsm app -- hyprpm reload"
+        "uwsm app -- blueman-applet"
+        "uwsm app -- kdeconnectd"
+        "uwsm app -- swayosd-server"
+      ];
+    };
   };
 }
