98b19cc59f38a3fd9a08a8952f48c918c07ad7ea
Andrew Fore <andrewrfore@gmail.com>
2025-11-12 13:00:32 -0500

    Updated imports;removed HM import

--- hosts/asus-amd-laptop/default.nix
+++ hosts/asus-amd-laptop/default.nix
@@ -8,8 +8,10 @@
     ../../modules/base/locale.nix
     ../../modules/base/packages.nix
     ../../modules/base/users.nix
-    ../../modules/home/hyprland
+
+    ../../modules/desktop/hyprland.nix
     ../../modules/desktop/sddm.nix
+
     ../../modules/hardware/amd-laptop.nix
   ];
 
