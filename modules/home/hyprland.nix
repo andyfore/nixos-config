36bbd3606259926f5de91ce4b2d08ca0d830f465
Andrew Fore <andrewrfore@gmail.com>
2026-03-21 10:14:16 -0400

    Updated look and feel, pt2b

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -94,8 +94,10 @@
         gaps_out = 20;
         border_size = 2;
         layout = "dwindle";
-        col.active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-        col.inactive_border = "rgba(595959aa)";
+        col =  {
+          active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+          inactive_border = "rgba(595959aa)";
+        };
         resize_on_border = false;
         allow_tearing = false;
       };
