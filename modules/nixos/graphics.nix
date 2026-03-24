9f9ac32fce38c215bffb7520892e2b958426f08f
Andrew Fore <andrewrfore@gmail.com>
2026-03-23 23:45:06 -0400

    Updated amd settings

--- modules/nixos/graphics.nix
+++ modules/nixos/graphics.nix
@@ -8,6 +8,13 @@
     enable32Bit = true;
   };
 
+  hardware.amdgpu = {
+    # load the amdgpu driver earlier
+    initrd.enable = true;
+    # rocm clr drivers
+    opencl.enable = true;
+  };
+
   services.libinput.enable = true;
 
   environment.sessionVariables = {
